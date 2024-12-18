dd2c51900758806827bae0dd96c52c751ff33118
snadiaf <88521545+snadiaf@users.noreply.github.com>
2024-12-17 20:17:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ with st.expander('Visualization'):
     st.info("Total Sales By Genre")
     st.bar_chart(data=df, x='genre', y='total_sales', color='genre')
 
-    top_publisher = df[['publisher', 'total_sales']].nlargest(15, 'total_sales')
-    st.info("Publisher of 15 Top Selling Games")    
-    st.bar_chart(data=top_publisher, x='publisher', y='total_sales', color='publisher')
+    #top_publisher = df[['publisher', 'total_sales']].nlargest(15, 'total_sales')
+    st.info("Total Sales By Publisher")    
+    st.bar_chart(data=df, x='publisher', y='total_sales', color='publisher')
 
 
     top_sellers = df[['title', 'total_sales']].nlargest(15, 'total_sales')
